52fd66543b7458abfc4dc1cc50c5a191cdbd503e
njona <jonathan.narvaez@mi.unc.edu.ar>
2025-05-17 19:38:11 -0300

    continuo agregando comentarios para mejor entendimiento del codigo

--- simpleServer.js
+++ simpleServer.js
@@ -15,6 +15,7 @@ const server = http.createServer(function (req, res) {
   // 200 indica que la solicitud fue exitosa y 'content-type' indica el tipo de contenido que se está enviando.
   // el metodo writeHead() se usa para enviar la respuesta HTTP al cliente.
   res.writeHead(200, {'content-type': 'text/html'});
+  // El método createReadStream() de fs se usa para leer un archivo de forma asíncrona.
   // Abre el archivo index.html como un stream de lectura (no carga todo el archivo en memoria de golpe)
   // .pipe(res) conecta ese stream directamente con la respuesta del servidor,
   // enviando el contenido del archivo al navegador.
