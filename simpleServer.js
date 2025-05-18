8e5f8977f81b82531b1afb6e0c1e4cae4cdb8173
njona <jonathan.narvaez@mi.unc.edu.ar>
2025-05-18 12:23:51 -0300

    continuo agregando comentarios para mejor entendimiento del codigo

--- simpleServer.js
+++ simpleServer.js
@@ -1,4 +1,5 @@
 // importa el modulo http de Nodejs que permite crear un servidor web
+// usa require porque es la forma en que Nodejs importa modulos
 const http = require('http');
 // Importa el modulo fs (File System), que permite trabajar con archivos (leer, escribir, etc)
 const fs = require('fs');
@@ -12,8 +13,10 @@ const PORT = 3000;
 // cada vez que llega una solicitud (req = request, res = response)
 const server = http.createServer(function (req, res) {
   // Responde al cliente enviando las cabeceras(headers)
+  // el metodo writeHead() se usa para enviar la respuesta HTTP al cliente. tiene dos argumentos:
+  // el primer argumento es el código de estado HTTP (200 significa OK)
+  // el segundo argumento es un objeto que contiene las cabeceras de la respuesta.
   // 200 indica que la solicitud fue exitosa y 'content-type' indica el tipo de contenido que se está enviando.
-  // el metodo writeHead() se usa para enviar la respuesta HTTP al cliente.
   res.writeHead(200, {'content-type': 'text/html'});
   // El método createReadStream() de fs se usa para leer un archivo de forma asíncrona.
   // Abre el archivo index.html como un stream de lectura (no carga todo el archivo en memoria de golpe)
